53f90a6855715dd65d0e27f41d18db4a99aa7ad9
Sander249 <sandervanveenen@gmail.com>
2017-03-29 11:32:35 +0200

    Leereenheden
    Pagina aangemaakt

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,6 +16,11 @@ Route::get('/', function()
 	return View::make('home');
 });
 
+Route::get( '/leereenheden', function()
+{
+	return View::make('leereenheden');
+});
+
 Route::get('/charts', function()
 {
 	return View::make('mcharts');
---
+++ resources/views/leereenheden.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.dashboard')
+@section('page_heading','Leereenheden')
+@section('section')
+<div class="col-sm-12">
+	<div class="row">
+		<h3> <span class="label label-success">Klaar</span> </h3>
+	</div>
+</div>
+<div class="col-sm-12">
+	<div class="row">
+		<h3> <span class="label label-warning">Actief</span> </h3>
+	</div>
+</div>
+<div class="col-sm-12">
+	<div class="row">
+		<h3> <span class="label label-default">Niet actief</span> </h3>
+	</div>
+</div>
+@stop
