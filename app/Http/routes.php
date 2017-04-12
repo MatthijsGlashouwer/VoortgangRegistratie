eae6c59ca1278c1590ec11364cd4e999336fb7a5
MatthijsGlashouwer <matthijsglashouwer@hotmail.com>
2017-04-12 09:02:59 +0200

    Auth Changes & database

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,6 +26,11 @@ Route::get('/createchecklist', function()
 	return View::make('createchecklist');
 });
 
+Route::get('/checklisttest', function()
+{
+	return View::make('checklisttest');
+});
+
 
 Route::get('/contact', function()
 {
@@ -108,3 +113,12 @@ Route::get('/documentation', function()
 {
 	return View::make('documentation');
 });
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
