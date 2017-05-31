30c21e6e25bf3226a29ba98f6f9d0923bdca128b
Sander249 <sandervanveenen@gmail.com>
2017-05-31 10:53:45 +0200

    commit nav

--- resources/views/layouts/plane.blade.php
+++ resources/views/layouts/plane.blade.php
@@ -12,10 +12,11 @@
 	<meta content="" name="description"/>
 	<meta content="" name="author"/>
 
+	<script src="{{ asset("assets/scripts/frontend.js") }}" type="text/javascript"></script>
+
 	<link rel="stylesheet" href="{{ asset("assets/stylesheets/styles.css") }}" />
 </head>
 <body>
 	@yield('body')
-	<script src="{{ asset("assets/scripts/frontend.js") }}" type="text/javascript"></script>
 </body>
 </html>
