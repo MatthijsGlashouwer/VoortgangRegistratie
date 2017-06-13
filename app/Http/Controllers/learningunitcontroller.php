cdb6524780d28ea5f6fde0ec10bfa10fddb0016f
MatthijsGlashouwer <matthijsglashouwer@hotmail.com>
2017-06-13 10:55:56 +0200

    test3
    test3

--- app/Http/Controllers/learningunitcontroller.php
+++ app/Http/Controllers/learningunitcontroller.php
@@ -1,25 +1,49 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\tbllearningunit;
+use App\tblusergroup;
+use Input;
 
 
-
-
-class LearningunitController extends Controller
-
+class learningunitcontroller extends Controller
 {
+    public function index()
+   {
+   		return view('leereenheden.index');
+   }
 
-public function index()
-{
-	return view('leereenheid');
+   public function create(Request $request)
+   {
+   		//* $tbllearningunit = new tbllearningunit; 
+
+   		// $Title = $request->('Title');
+
+   		// $tbllearningunit->NLQF = Input::get('NLQF', false);
+
+   		// $tbllearningunit->save(); 
+
+   		return view('leereenheden.create')->with('tbllearningunit',$tbllearningunit);
+
+   }
+
+   public function view()
+   {
+   		$tbllearningunit = tbllearningunit::find([2,5]);
+   		return view('leereenheden.view')->with('tbllearningunit',$tbllearningunit);
+
+   }
+   public function edit()
+   {
+   		return view('leereenheden.edit');
+
+   }
+
 }
-  
-public function leereenhedenView(){
-        $learningunit = tbllearningunit::all();
-        return view ('leereenheid')->with('leereenheid',$tbllearningunit);
-    }
 
 
 
-
-}
--- app/tbllearningunit.php
+++ app/tbllearningunit.php
@@ -6,20 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class tbllearningunit extends Model
 {
-<<<<<<< HEAD
 	protected $table = 'tbllearningunit';
-=======
-    protected $table =	"tbllearningunit";
-    
-    public $timestamps = false;
-
-    $Title = App\tbllearningunit::all();
-
-    foreach ($Title as $flight) {
-
-    	echo $Title->name;
-
-    }
-
->>>>>>> 6b7b553861fd209c750a6916ef4d6c298609ff1d
 }
