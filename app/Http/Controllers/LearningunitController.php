42507bfc8dd88a21d1dbed12e1a620c3082912ec
MatthijsGlashouwer <matthijsglashouwer@hotmail.com>
2017-06-20 09:13:53 +0200

    t

--- app/Http/Controllers/LearningunitController.php
+++ app/Http/Controllers/LearningunitController.php
@@ -1,25 +1,76 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\tbllearningunit;
+use App\tblusergroup;
+use Input;
+use App;
 
 
-
-
-class LearningunitController extends Controller
-
+class learningunitcontroller extends Controller
 {
+   public function index()
+      {
+   		return view('leereenheden.index');
+      }
 
-public function index()
-{
-	return view('leereenheid');
-}
-  
-public function leereenhedenView(){
-        $learningunit = tbllearningunit::all();
-        return view ('leereenheid')->with('leereenheid',$tbllearningunit);
+   public function insertform()
+    {
+      return view('leereenheden.create');
     }
 
+   public function store(Request $request)
+   {
+
+      $tbllearningunit = $request->input('LEname');
+      // DB::insert('',[$tbllearningunit]);
+   	/*	$tbllearningunit = new tbllearningunit;
+
+         $tbllearningunit->Title = $request->Title;
+         
+         $tbllearningunit->NLQF = $request->NLQF;
+         
+         $tbllearningunit->Cohort = $request->Cohort;
+         
+         $tbllearningunit->Crebo = $request->Crebo;
+   		
+         $tbllearningunit->save(); */
+
+         return view('leereenheden.create')->with('tbllearningunit',$tbllearningunit); 
+
+   }
+
+   public function view()
+   {
+   		$tbllearningunit = tbllearningunit::find([2,5,3]);
+   		return view('leereenheden.view')->with('tbllearningunit',$tbllearningunit);
+
+   }
+   public function edit()
+   {     
+         $tbllearningunit = App\tbllearningunit::find(2);
+
+         $tbllearningunit->Title = 'test';
+
+         $tbllearningunit->save();
+
+   		return view('leereenheden.edit');
+
+   }
+
+   public function delete()
+   {
+         $tbllearningunit = App\tbllearningunit::find(8);
+
+         $tbllearningunit->delete();
 
 
-
+   }
 }
+
+
+
