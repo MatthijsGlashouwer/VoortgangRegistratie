f3699100e8927b871ff921532fc2d7d9a059d5a4
MatthijsGlashouwer <matthijsglashouwer@hotmail.com>
2017-07-05 09:36:57 +0200

    t
    t

--- app/Http/Controllers/LearningunitController.php
+++ app/Http/Controllers/LearningunitController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\tbllearningunit;
 use App\tblusergroup;
-use Input;
 use App;
+use Validator;
 
 
 class learningunitcontroller extends Controller
@@ -25,29 +26,38 @@ class learningunitcontroller extends Controller
 
    public function store(Request $request)
    {
+        $rules = array(
+            'Title' => 'required',
+            'Description' =>  'required',
+            'NLQF'  => 'required',
+            'Crebo' => 'required',
+            'Cohort'  => 'require'
+        );
 
-      $tbllearningunit = $request->input('LEname');
-      // DB::insert('',[$tbllearningunit]);
-   	/*	$tbllearningunit = new tbllearningunit;
 
-         $tbllearningunit->Title = $request->Title;
-         
-         $tbllearningunit->NLQF = $request->NLQF;
-         
-         $tbllearningunit->Cohort = $request->Cohort;
-         
-         $tbllearningunit->Crebo = $request->Crebo;
-   		
-         $tbllearningunit->save(); */
-
-         return view('leereenheden.create')->with('tbllearningunit',$tbllearningunit); 
-
+        $validator = Validator::make($request->all(), $rules);
+        
+        if ($validator->fails()) {
+            return Redirect('leereenheden/create')->withInput()->withErrors($validator->messages());
+        }
+        else {
+        $tbllearningunit = new tbllearningunit;
+        $tbllearningunit->Title = $request["Title"];
+        $tbllearningunit->NLQF = $request["NLQF"];
+        $tbllearningunit->Crebo = $request["Crebo"];
+        $tbllearningunit->Cohort = $request["Cohort"];
+        $tbllearningunit->save();
+        if($tbllearningunit->save()){
+            return Redirect('leereenheden/view')->withMessage($tbllearningunit->Title . " is toegevoegd.");
+        }
+        // return view('leereenheden.create')->with('tbllearningunit',$tbllearningunit); 
+        }
    }
 
    public function view()
    {
-   		$tbllearningunit = tbllearningunit::find([2,5,3]);
-   		return view('leereenheden.view')->with('tbllearningunit',$tbllearningunit);
+   		$tbllearningunit = tbllearningunit::all();
+   		return view('leereenheden.view')->with('klaar',$tbllearningunit);
 
    }
    public function edit()
