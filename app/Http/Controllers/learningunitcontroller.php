256ca41969ff6e6bdc88a543c9c3b681a91330be
MatthijsGlashouwer <matthijsglashouwer@hotmail.com>
2017-07-05 09:41:49 +0200

    Leereenheden Controller + Views
    test

--- app/Http/Controllers/learningunitcontroller.php
+++ app/Http/Controllers/learningunitcontroller.php
@@ -3,40 +3,83 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\tbllearningunit;
 use App\tblusergroup;
-use Input;
+use App;
+use Validator;
 
 
 class learningunitcontroller extends Controller
 {
-    public function index()
-   {
+   public function index()
+      {
    		return view('leereenheden.index');
-   }
+      }
 
-   public function create()
+   public function insertform()
+    {
+      return view('leereenheden.create');
+    }
+
+   public function store(Request $request)
    {
+        $rules = array(
+            'Title' => 'required',
+            'Description' =>  'required',
+            'NLQF'  => 'required',
+            'Crebo' => 'required',
+            'Cohort'  => 'require'
+        );
 
-   		$tbllearningunit = tbllearningunit::all();
-   		return view('leereenheden.create')->with('tbllearningunit',$tbllearningunit);
 
+        $validator = Validator::make($request->all(), $rules);
+        
+        if ($validator->fails()) {
+            return Redirect('leereenheden/create')->withInput()->withErrors($validator->messages());
+        }
+        else {
+        $tbllearningunit = new tbllearningunit;
+        $tbllearningunit->Title = $request["Title"];
+        $tbllearningunit->NLQF = $request["NLQF"];
+        $tbllearningunit->Crebo = $request["Crebo"];
+        $tbllearningunit->Cohort = $request["Cohort"];
+        $tbllearningunit->save();
+        if($tbllearningunit->save()){
+            return Redirect('leereenheden/view')->withMessage($tbllearningunit->Title . " is toegevoegd.");
+        }
+        // return view('leereenheden.create')->with('tbllearningunit',$tbllearningunit); 
+        }
    }
 
    public function view()
    {
-   		$tbllearningunit = tbllearningunit::find([2,5]);
-   		return view('leereenheden.view')->with('tbllearningunit',$tbllearningunit);
+   		$tbllearningunit = tbllearningunit::all();
+   		return view('leereenheden.view')->with('klaar',$tbllearningunit);
 
    }
    public function edit()
-   {
+   {     
+         $tbllearningunit = App\tbllearningunit::find(2);
+
+         $tbllearningunit->Title = 'test';
+
+         $tbllearningunit->save();
+
    		return view('leereenheden.edit');
 
    }
 
+   public function delete()
+   {
+         $tbllearningunit = App\tbllearningunit::find(8);
+
+         $tbllearningunit->delete();
+
+
+   }
 }
 
 
