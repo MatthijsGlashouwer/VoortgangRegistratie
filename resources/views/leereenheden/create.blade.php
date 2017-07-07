8b9be9e84d3db0824e2e80fe2698faffafbd88da
MatthijsGlashouwer <matthijsglashouwer@hotmail.com>
2017-07-07 11:26:56 +0200

    Sidebar+Createview

--- resources/views/leereenheden/create.blade.php
+++ resources/views/leereenheden/create.blade.php
@@ -10,22 +10,27 @@
 		<form action="store" method="POST">
 		<label for="Title">Title</label>
 		<input type="text" name="Title" id="Title">
+		<br>
+		<label for="Link">Link</label>
+		<input type="text" name="Link" id="Link">
+		<br>
+		<label for="NLQF">NLQF</label>
+		<input type="text" name="NLQF" id="NLQF">
+		<br>
+		<label for="Cohort">Cohort</label>
+		<input type="text" name="Cohort" id="Cohort">
+		<br>
+		<label for="Crebo">Crebo</label>
+		<input type="text" name="Crebo" id="Crebo">
+		<br>
+		<input type="submit" name="submit" value="Toevoegen">	
 	</div>
 </div>
 
 
 
 
-<!-- Buttons -->
-<div class="col-xs-12" style="margin-bottom: 10px; margin-top:10px">
-	<div class="col-xs-2">
-		<button class=" btn btn-default" id="buttonStartProject">
-			Leereenheid opslaan
-		</button>
-	</div>
 
-
-</div>
 
 </html>
 @stop
