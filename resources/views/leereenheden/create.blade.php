7ff07cb1f87df51366ced1cf8c46b8de0c297dfb
MatthijsGlashouwer <matthijsglashouwer@hotmail.com>
2017-07-07 12:04:33 +0200

    LE

--- resources/views/leereenheden/create.blade.php
+++ resources/views/leereenheden/create.blade.php
@@ -7,23 +7,25 @@
 <!-- Title-->
 <div class="col-xs-12">
 	<div class="col-xs-4">
-		<form action="store" method="POST">
-		<label for="Title">Title</label>
-		<input type="text" name="Title" id="Title">
-		<br>
-		<label for="Link">Link</label>
-		<input type="text" name="Link" id="Link">
-		<br>
-		<label for="NLQF">NLQF</label>
-		<input type="text" name="NLQF" id="NLQF">
-		<br>
-		<label for="Cohort">Cohort</label>
-		<input type="text" name="Cohort" id="Cohort">
-		<br>
-		<label for="Crebo">Crebo</label>
-		<input type="text" name="Crebo" id="Crebo">
-		<br>
-		<input type="submit" name="submit" value="Toevoegen">	
+		<form action="view/1" method="POST">
+			{{ csrf_field() }}
+			<label for="Title">Title</label>
+			<input type="text" name="Title" id="Title">
+			<br>
+			<label for="Link">Link</label>
+			<input type="text" name="Link" id="Link">
+			<br>
+			<label for="NLQF">NLQF</label>
+			<input type="text" name="NLQF" id="NLQF">
+			<br>
+			<label for="Cohort">Cohort</label>
+			<input type="text" name="Cohort" id="Cohort">
+			<br>
+			<label for="Crebo">Crebo</label>
+			<input type="text" name="Crebo" id="Crebo">
+			<br>
+			<input type="submit" name="submit" value="Toevoegen">	
+		</form>
 	</div>
 </div>
 
