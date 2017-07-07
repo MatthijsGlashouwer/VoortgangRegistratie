53d3bf1134796ac6aec994eaec36e4960f6a1fef
MatthijsGlashouwer <matthijsglashouwer@hotmail.com>
2017-07-07 12:49:46 +0200

    edit

--- resources/views/leereenheden/edit.blade.php
+++ resources/views/leereenheden/edit.blade.php
@@ -1,13 +1,38 @@
+@extends('layouts.dashboard')
+@section('page_heading','Leereenheid aanpassen')
+@section('content')
 
-<!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
-		<h1>edit template</h1>
-</body>
+
+<!-- Title-->
+<div class="col-xs-12">
+	<div class="col-xs-4">
+		<form action="view/1" method="POST">
+			{{ csrf_field() }}
+			<label for="Title">Title</label>
+			<input type="text" name="Title" id="Title">
+			<br>
+			<label for="Link">Link</label>
+			<input type="text" name="Link" id="Link">
+			<br>
+			<label for="NLQF">NLQF</label>
+			<input type="text" name="NLQF" id="NLQF">
+			<br>
+			<label for="Cohort">Cohort</label>
+			<input type="text" name="Cohort" id="Cohort">
+			<br>
+			<label for="Crebo">Crebo</label>
+			<input type="text" name="Crebo" id="Crebo">
+			<br>
+			<input type="submit" name="submit" value="Toevoegen">	
+		</form>
+	</div>
+</div>
+
+
+
+
+
+
 </html>
-
-
-
+@stop
